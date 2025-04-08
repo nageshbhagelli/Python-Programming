1f379bc4435e41759073900df93ebc29ea7ad7ee
nageshbhagelli <bhagellinagesh@gmail.com>
2025-04-08 18:47:20 +0530

    files added

---
+++ week_4/p8.py
@@ -0,0 +1,15 @@
+#8. Write a Python program that demonstrates the use of the iter() function on a list and
+# manually retrieves elements using the next() function until a StopIteration exception is encountered.
+
+def manual_next(lst):
+    iterator = iter(lst)
+    while True:
+        try:
+            print(next(iterator))
+        except StopIteration:
+            print("End of iteration")
+            break
+
+# Example usage
+my_list = [10, 20, 30, 40]
+manual_next(my_list)

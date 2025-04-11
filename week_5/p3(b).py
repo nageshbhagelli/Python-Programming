50572099787bca5076cd951e0d9c5d007ef2a4aa
nageshbhagelli <bhagellinagesh@gmail.com>
2025-04-11 22:25:58 +0530

    files added

---
+++ week_5/p3(b).py
@@ -0,0 +1,12 @@
+# 3.b Count Words in a File:
+# Write a Python program that reads a file and counts the number of words in it.
+
+filename = "source.txt"
+
+try:
+    with open(filename, "r") as file:
+        content = file.read()
+        word_count = len(content.split())
+    print("Word count:", word_count)
+except FileNotFoundError:
+    print("File not found.")

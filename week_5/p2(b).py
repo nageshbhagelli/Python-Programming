4525556e94d07c8b5842ca0a9542f803312efa2a
nageshbhagelli <bhagellinagesh@gmail.com>
2025-04-11 22:24:06 +0530

    files added

---
+++ week_5/p2(b).py
@@ -0,0 +1,8 @@
+# 2.b Using time Module:
+# Write a Python program that prints the current time, pauses execution for 5 seconds using sleep(), and then prints a message.
+
+import time
+
+print("Current time:", time.ctime())
+time.sleep(5)
+print("Execution resumed after 5 seconds.")

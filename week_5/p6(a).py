e0f6bce16c7da400468c1cfe08f2e23177307e32
nageshbhagelli <bhagellinagesh@gmail.com>
2025-04-14 11:16:47 +0530

    files added

---
+++ week_5/p6(a).py
@@ -0,0 +1,13 @@
+# 6.a Reading a JSON File:
+# Write a Python program that reads a JSON file and prints the content in a structured format.
+
+import json
+
+filename = "data.json"
+
+try:
+    with open(filename, "r") as jsonfile:
+        data = json.load(jsonfile)
+    print(json.dumps(data, indent=4))
+except FileNotFoundError:
+    print("JSON file not found.")

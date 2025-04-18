8797a43b2442090fef55a0fbc6aefd94cf0ef019
nageshbhagelli <bhagellinagesh@gmail.com>
2025-04-18 22:25:27 +0530

    files added

---
+++ week_5/p4(b).py
@@ -0,0 +1,12 @@
+# 4.b Writing a File in Binary Mode:
+# Write a Python program that opens an image file in binary mode and writes it to a new file.
+
+source_image = "week_5/python.jpeg"
+destination_image = "copy.jpg"
+
+try:
+    with open(source_image, "rb") as src, open(destination_image, "wb") as dest:
+        dest.write(src.read())
+    print("Image copied successfully.")
+except FileNotFoundError:
+    print("Source image not found.")

1252f54a43264729d12ebe90eac0cfeaec685a85
nageshbhagelli <bhagellinagesh@gmail.com>
2025-04-18 22:26:45 +0530

    files added\

--- week_5/p3(a).py
+++ week_5/p3(a).py
@@ -2,7 +2,7 @@
 # Write a Python program that reads a file and writes its contents to another file.
 
 source_file = "week_5/source.txt"
-destination_file = "copy.txt"
+destination_file = "week_5/copy.txt"
 
 try:
     with open(source_file, "r") as src, open(destination_file, "w") as dest:

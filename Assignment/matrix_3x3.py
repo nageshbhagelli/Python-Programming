9cd7f842b3ca0cdb341c1bfc005a79312879c13d
nageshbhagelli <bhagellinagesh@gmail.com>
2025-04-26 19:34:42 +0530

    files added

---
+++ Assignment/matrix_3x3.py
@@ -0,0 +1,11 @@
+#1. Write a Python program to create a 3x3 matrix using a nested list.
+
+matrix_3x3 = [
+    [9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1]
+]
+
+# Printing the matrix
+for row in matrix_3x3:
+    print(row)

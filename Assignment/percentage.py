0617e76460d19c9114464ca20315782b73ccd688
nageshbhagelli <bhagellinagesh@gmail.com>
2025-04-26 19:38:24 +0530

    files added

--- Assignment/percentage.py
+++ Assignment/percentage.py
@@ -1,1 +1,8 @@
 #2. How would you calculate the percentage of a student if the total marks are 500 and the student scored 375?
+
+total_marks = 500
+marks_scored = 375
+
+percentage = (marks_scored / total_marks) * 100
+
+print(f"Percentage of the student is {percentage}%")

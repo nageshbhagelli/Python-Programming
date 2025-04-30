a7f0e364ad55b997556bf35c4acccdca4479d837
nageshbhagelli <bhagellinagesh@gmail.com>
2025-04-30 22:28:32 +0530

    files added

--- Assignment/a8.py
+++ Assignment/a8.py
@@ -1,3 +1,7 @@
 #8. How would you calculate the average of a given list of numbers?
 
-num_list = [11, 22, 33, 44, 55, 66, ]
+num_list = [11, 22, 33, 44, 55, 66, 77]
+
+average = sum(num_list)/len(num_list)
+
+print(f"Average : {average}")

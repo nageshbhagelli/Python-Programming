1d4369f0c327d56509ab044d2adffb6e47d0655b
nageshbhagelli <bhagellinagesh@gmail.com>
2025-05-01 22:23:53 +0530

    files added

--- Assignment/a9.py
+++ Assignment/a9.py
@@ -1,2 +1,13 @@
 #9. Write a Python function that accepts a matrix and returns the sum of all its elements
 
+def sum_of_matrix(matrix):
+    total_sum = sum(sum(row) for row in matrix)
+    return total_sum
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+result = sum_of_matrix(matrix)
+print(f"The sum of all elements in the matrix is {result}")

3033c508e67c2d46d4f1888d3c3bc3ce67b67178
nageshbhagelli <bhagellinagesh@gmail.com>
2025-05-02 22:41:46 +0530

    files added

--- Assignment/a10.py
+++ Assignment/a10.py
@@ -1,1 +1,14 @@
 #10. Write a program to calculate the difference between the highest and lowest values in each list.
+
+matrix = [
+    [3, 8, 1],
+    [1, 6, 9],
+    [7, 2, 5]
+]
+
+# Calculate the difference between the highest and lowest value in each list
+difference = [max(row) - min(row) for row in matrix]
+
+# Using a for loop to print the differences
+for i in range(len(difference)):
+    print(f"Difference between highest and lowest values in list {i + 1}: {difference[i]}")

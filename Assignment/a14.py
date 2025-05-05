c39843d9089b333bb956947f52ba42ae72f36e34
nageshbhagelli <bhagellinagesh@gmail.com>
2025-05-05 22:16:13 +0530

    files added

--- Assignment/a14.py
+++ Assignment/a14.py
@@ -1,1 +1,10 @@
 # 14. Write a Python program to calculate the percentage increase between two numbers (old and new).
+
+def percentage_increase(old, new):
+    return ((new - old) / old) * 100
+
+old_value = 50
+new_value = 75
+
+result = percentage_increase(old_value, new_value)
+print(f"The percentage increase is: {result}%")

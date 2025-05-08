8788c1052bbf980030bc7053bb91a16c523ea3c9
nageshbhagelli <bhagellinagesh@gmail.com>
2025-05-08 22:50:14 +0530

    add files

--- Assignment/a16.py
+++ Assignment/a16.py
@@ -8,3 +8,4 @@ def sum_of_diagonal(matrix):
 matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 result = sum_of_diagonal(matrix)
 print(f"The sum of the diagonal elements of a square matrix is: {result}")
+

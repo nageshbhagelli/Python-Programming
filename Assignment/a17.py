56e789f15a460c4ccd515220b49245dc664dc6c0
nageshbhagelli <bhagellinagesh@gmail.com>
2025-05-09 22:58:25 +0530

    files added

--- Assignment/a17.py
+++ Assignment/a17.py
@@ -1,1 +1,11 @@
 # 17. Write a Python function to find the most frequent element in a list of numbers.
+
+from collections import Counter
+
+def most_frequent(numbers):
+    count = Counter(numbers)
+    return count.most_common(1)[0][0]
+
+numbers = [1, 2, 2, 3, 3, 3, 4]
+result = most_frequent(numbers)
+print(f"The most frequent element is: {result}")

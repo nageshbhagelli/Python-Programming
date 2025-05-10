239b7c9682141c7d7fbc77414ab928e6941a5fe7
nageshbhagelli <bhagellinagesh@gmail.com>
2025-05-10 22:03:34 +0530

    files added

---
+++ Assignment/a18.py
@@ -0,0 +1,3 @@
+# 18. Create a program to simulate a "high-low" number guessing game. The program generates 
+# a random number, and the user must guess whether the next number will be higher or lower.
+

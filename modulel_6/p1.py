549d3615d1482ea9094b625c0e2f9c7687c1fbf8
nageshbhagelli <bhagellinagesh@gmail.com>
2025-05-14 22:31:25 +0530

    files added

--- modulel_6/p1.py
+++ modulel_6/p1.py
@@ -1,1 +1,7 @@
 # 1. Write a Python program to create a NumPy array of 5 zeros and another array of 5 ones.
+
+import numpy as np
+zeros = np.zeros(5)
+ones = np.ones(5)
+print("1. Zeros:", zeros)
+print("   Ones:", ones)

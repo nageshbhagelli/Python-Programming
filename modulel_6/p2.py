2816531335cf38d67efe07b961f55afcc5480094
nageshbhagelli <bhagellinagesh@gmail.com>
2025-05-16 22:28:01 +0530

    files added

--- modulel_6/p2.py
+++ modulel_6/p2.py
@@ -1,1 +1,5 @@
 # 2. Write a Python program to create a NumPy array with values between 1 and 10, spaced by 0.5.
+
+import numpy as np
+spaced_array = np.arange(1, 10.5, 0.5)
+print("\n2. Array from 1 to 10 spaced by 0.5:", spaced_array)

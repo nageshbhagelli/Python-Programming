f8fc9e01e55b52f313ffa1f885903fdddb4c3b43
nageshbhagelli <bhagellinagesh@gmail.com>
2025-05-17 21:41:09 +0530

    files added

--- modulel_6/p3.py
+++ modulel_6/p3.py
@@ -1,1 +1,7 @@
 # 3. Write a Python program to concatenate two NumPy arrays.
+
+import numpy as np
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+concatenated = np.concatenate((arr1, arr2))
+print("\n3. Concatenated array:", concatenated)

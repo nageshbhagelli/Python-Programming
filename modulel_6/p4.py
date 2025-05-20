b55f3abf5349e29c719f43e32904f2a86e1c2446
nageshbhagelli <bhagellinagesh@gmail.com>
2025-05-20 21:58:40 +0530

    files added

--- modulel_6/p4.py
+++ modulel_6/p4.py
@@ -1,1 +1,6 @@
 # 4. Write a Python program to split a NumPy array into three equal-sized subarrays.
+
+import numpy as np
+split_array = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8])
+subarrays = np.split(split_array, 3)
+print("\n4. Split into three subarrays:", subarrays)

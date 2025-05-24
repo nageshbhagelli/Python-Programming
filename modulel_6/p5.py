d3af2f582fa49b69becfbeaf016d080f6be34119
nageshbhagelli <bhagellinagesh@gmail.com>
2025-05-24 21:50:22 +0530

    files added

--- modulel_6/p5.py
+++ modulel_6/p5.py
@@ -1,1 +1,6 @@
 # 5. Write a Python program to extract all even numbers from a NumPy array.
+
+import numpy as np
+data = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+even_numbers = data[data % 2 == 0]
+print("\n5. Even numbers:", even_numbers)

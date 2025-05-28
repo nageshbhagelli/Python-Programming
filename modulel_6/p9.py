e421d4c46096f13ecc14d7916522ac321f2471cf
nageshbhagelli <bhagellinagesh@gmail.com>
2025-05-28 17:48:08 +0530

    files added

---
+++ modulel_6/p9.py
@@ -0,0 +1,6 @@
+# 9. Write a Python program to generate a 3×3 matrix with random values between 0 and 1.
+
+import numpy as np
+
+random_matrix = np.random.rand(3, 3)
+print("\nRandom 3x3 matrix:\n", random_matrix)

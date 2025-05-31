be4a2e21835c6b4ebf55900f8f5db7b8c42a3d3b
nageshbhagelli <bhagellinagesh@gmail.com>
2025-05-31 21:37:48 +0530

    files added

--- modulel_6/p10.py
+++ modulel_6/p10.py
@@ -1,1 +1,6 @@
 # 10. Write a Python program to create a diagonal matrix with specified values along the diagonal.
+
+import numpy as np
+diagonal_values = [10, 20, 30]
+diagonal_matrix = np.diag(diagonal_values)
+print("\n10. Diagonal matrix:\n", diagonal_matrix)
